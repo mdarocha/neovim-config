6413b5fb9cba488e8eb30ee1a73a98fe279a843a
mdarocha <git@mdarocha.pl>
2025-01-21 20:05:19 +0100

    Add readme, format

---
+++ README.md
@@ -0,0 +1,6 @@
+# neovim-config
+
+My own custom neovim configuration.
+
+Uses [Home Manager](https://github.com/nix-community/home-manager/)
+to manage neovim configuration and plugins.
--- flake.nix
+++ flake.nix
@@ -1,4 +1,13 @@
 {
   description = "my custom neovim config";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+    };
 }
