0d4a54c12ed4ef815cd864c510467f5fb6d87208
mdarocha <git@mdarocha.pl>
2025-05-04 12:39:00 +0200

    Fix LSP configs

--- config/configs/lsp.lua
+++ config/configs/lsp.lua
@@ -107,7 +107,7 @@ require("lazy-lsp").setup {
     nix = { "nil_ls" },
   },
   -- overrides for some servers
-  config = {
+  configs = {
     omnisharp = {
       cmd = {
         "OmniSharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()),
@@ -133,11 +133,48 @@ require("lazy-lsp").setup {
       }
     },
     lua_ls = {
+      on_init = function(client)
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most
+            -- likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Tell the language server how to find Lua modules same way as Neovim
+            -- (see `:h lua-module-load`)
+            path = {
+              'lua/?.lua',
+              'lua/?/init.lua',
+            },
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- Depending on the usage, you might want to add additional paths
+              -- here.
+              -- '${3rd}/luv/library'
+              -- '${3rd}/busted/library'
+            }
+            -- Or pull in all of 'runtimepath'.
+            -- NOTE: this is a lot slower and will cause issues when working on
+            -- your own configuration.
+            -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+            -- library = {
+            --   vim.api.nvim_get_runtime_file('', true),
+            -- }
+          }
+        })
+      end,
       settings = {
         Lua = {
+         runtime = {
+            version = 'LuaJIT',
+            path = vim.split(package.path, ';'),
+          },
           diagnostics = {
-            globals = { "vim" }
-          }
+            globals = { 'vim' },
+          },
         }
       }
     }
